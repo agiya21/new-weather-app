1a0cb61545f11f06994e22af054fa14d6175e3c1
agiya21 <agiya72@gmail.com>
2021-03-03 20:41:09 +0700

    feat: 🎸 Adding new Components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <h1>This is Weather Forecast Web</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function SearchBar(){
+    return(
+        <div>This is Search Bar</div>
+    )
+}
+
+export default SearchBar
